9050ba265b1ba2c32b55343cab88f62f80dfadb6
minderama <lndrferreiras@gmail.com>
2025-05-10 13:59:06 +0000

    Nivel 3

--- xadrez.c
+++ xadrez.c
@@ -1,33 +1,63 @@
 #include <stdio.h>
 
+void movertorre(int casas) {
+    if (casas > 0) {
+        printf("Direita\n");
+        movertorre(casas - 1);
+    }
+}
+
+void moverbispo(int casas){
+    if (casas > 0) {
+        for (int v = 0; v < 1; v++) {
+            for (int h = 0; h < 1; h++) {
+                for (int h = 0; h < 1; h++) {
+                    printf("Cima, Direita\n");
+                }
+            }
+        }
+        moverbispo(casas - 1);
+    }
+}
+
+void moverrainha (int casas) {
+    if (casas > 0) {
+        printf("Esquerda\n");
+        moverrainha(casas - 1);
+    }
+}
+
 int main() {
     int torre = 5;
     int bispo = 5;
     int rainha = 8;
-    
-    printf("Movimento da Torre\n");
 
-    for(int i = 1; i <= torre; i++){
-        printf("Direita\n");
-    }
+    printf("Movimento da Torre\n");
+    movertorre(torre);
 
     printf("Movimento do Bispo\n");
-    int a = 1;
-
-    while (a <= bispo){
-        printf("Cima, direita\n");
-        a++;
-    }
-
+    moverbispo(bispo);
+   
     printf("Movimento da Rainha\n");
-    int b = 1;
-    do{
-        printf("Esqueda\n");
-        b++;
-    } while (b <= rainha);
+    moverrainha(rainha);
 
-    
+    printf("Movimento do Cavalo\n");
+    int movcavalovertical = 2;
+    int movcavalohorizontal = 1;
 
+    for (int i = 1; i <= movcavalovertical; i++) {
+        if (i == 2) {
+            for (int j = 1; j <= movcavalohorizontal; j++) {
+                if (j == 1) {
+                    print("Direita\n");
+                    continue;
+                }
+                break;
+
+            }
+        }
+        printf("Cima\n");
+    }
 
     return 0;
 }
